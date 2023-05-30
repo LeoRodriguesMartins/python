24d278649749f316c3081b7b383e9f2aa0847a17
Leonardo Rodrigues <76709141+LeoRodriguesMartins@users.noreply.github.com>
2023-05-30 15:33:36 -0300

    Add files via upload

---
+++ estudopython.py
@@ -0,0 +1,110 @@
+"""
+
+print("texto")  #exibira um texto na tela
+
+
+if 5 > 8:
+    print("Cinco é maior")
+else:
+    print("Cinco é menor")
+        print("testando espaço") #exibirá um erro por conta da identação
+                                 #deve seguir a identação dos comandos anteriores
+
+"""
+        
+#comentário em python
+
+"""
+COMENTARIO
+MULTILINHAS
+NO PYTHON
+"""
+
+"""
+    Regras para variáveis ​​Python:
+    Um nome de variável deve começar com uma letra ou o caractere de sublinhado
+    Um nome de variável não pode começar com um número
+    Um nome de variável pode conter apenas caracteres alfanuméricos e sublinhados (Az, 0-9 e _ )
+    Os nomes das variáveis ​​diferenciam maiúsculas de minúsculas (idade, Idade e IDADE são três variáveis ​​diferentes)
+    Um nome de variável não pode ser nenhuma das palavras-chave do Python.
+"""
+
+"""
+#Variaveis em Python são declaradas atribuindo valor a elas
+#exemplo:
+
+x = 5 #variavel do tipo int (inteiro)
+y = "Jhon" #variavel do tipo string (texto)
+
+print(x)
+print(y)
+
+
+
+#Variavies podem mudar seu tipo no meio do código
+#exemplo:
+
+x = 4 #tipo int
+x = "Texto" #agora mudou para string
+
+print(x)
+
+
+#para especificar o tipo de dados em uma variável, pode ser usado a conversão
+#exemplo:
+x = str(3) #será '3' por string/texto
+y = int(3) #será 3 por inteiro
+z = float(3) #z será 3.0 por número flutuante
+
+print(x)
+print(y)
+print(z)
+
+
+#Para obter o tipo de dados colocado em uma variável utilizando type()
+
+x = 5
+y = "john"
+
+print(type(x))
+print(type(y))
+
+
+# string podem ser declaradas com aspas simples 'texto' ou aspas duplas "texto"
+
+x = 'aspas simples'
+y = "aspas duplas"
+
+print(x)
+print(y)
+
+
+
+#o nome de variáveis se diferenciam entre minúsculas e maiúsculas
+a = 4
+A = "Sally"
+
+print(a)
+print(A)
+
+
+
+#O Python permite que você atribua valores a várias variáveis ​​em uma linha:
+
+x, y, z = "Orange", "Banana", "Morango"
+
+print(y)
+print(x)
+print(z)
+
+
+
+#Perimite atribuir o mesmo valor a várias variáveis ​​em uma linha:
+
+x, y, z = "Orange"
+print(y)
+print(x)
+print(z)
+
+"""
+
