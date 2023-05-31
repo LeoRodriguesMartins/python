473259a9700f7ed350171b780a81a3d54613a152
Leonardo Rodrigues <76709141+LeoRodriguesMartins@users.noreply.github.com>
2023-05-31 16:13:16 -0300

    Add files via upload
    Variáveis, variáveis locais e globais

--- estudopython.py
+++ estudopython.py
@@ -99,12 +99,123 @@ print(z)
 
 
 
-#Perimite atribuir o mesmo valor a várias variáveis ​​em uma linha:
+#Permite atribuir o mesmo valor a várias variáveis ​​em uma linha:
 
 x, y, z = "Orange"
 print(y)
 print(x)
 print(z)
 
+
+#caso possua uma coleção de valores em uma list, tupla... é possível extrai os valores e distribuir em variáveis. É chamado de descompactação
+
+fruits = ["apple", "banana", "morango"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+
+#Variaveis de saída. A função print() é usada frequentemente para variáveis de saida
+#exemplo:
+
+x = "Python var saída"
+print(x)
+
+# é possivel gerar várias variáveis, separadas por uma vírgula:
+#obs.: neste caso, note que já será com exibido o espaçamento nas palavras
+x = "Python"
+y = "is"
+z = "awesome"
+
+print(x,y,z)
+
+# pode ser utilizado + para gerar várias variáveis
+#obs.: note que neste caso será
+x = "Python "
+y = "is "
+z = "awseome"
+print(x + y + z)
+
+# Para números, o + funciona como operador matemático realizando a soma das váriavies
+#exemplo:
+x = 5
+y = 10
+print(x + y)
+
+# o simbolo + não permita a utilização quando os valores das variáveis são de tipos direntes
+#exemplo:  
+
+x = 5 #tipo int
+y = "Jhon" #tipo string
+print(x + y)
+#gera erro
+
+
+# para gerar variáveis de diferentes valores no print() o ideal é a utilização de ,
+#exemplo:
+
+x = 5
+y = "Jhon"
+print(x, y)
+
+
+
+#Váriáveis criadas fora de uma função são conhecidas como variáveis globais
+#Variáveis globais podem ser usados por todos, tanto dentro quanto fora de funções
+
+#exemplo:
+
+x = "awesome"
+
+def myFunc():
+    print("Python is " + x)
+myFunc()
+
+
+
+#se for criado uma variável com o mesmo nome dentro de uma função,
+#esta variável será local, e só poderá ser utilizada dentro da função
+
+#a variável local permanecerá como global
+
+#exemplo:
+
+x = "awesome"
+
+def myFunc():
+    x = "fantastic"
+    print("Python is " + x)
+    
+myFunc()
+
+print("Python is " + x)
+
+
+
+
+#para criar um variável global dentro de uma função, pode-se utilizar a palavra-chave: global
+#exemplo:
+
+def myFunc():
+    global x    #indica que a variavel será global
+    x = "fantastic" #valor e tipo da variável
+myFunc()
+
+print("Python is", x)
+
+
+
+#a palavra-chave 'global' pode ser utilizada para trocar o valor de uma variável global dentro de uma função
+#exemplo:
+
+x = "awesome"
+def myFunc():
+    global x
+    x = "fantastic"
+myFunc()
+
+print("Python is " + x)
+
 """
-teste git
